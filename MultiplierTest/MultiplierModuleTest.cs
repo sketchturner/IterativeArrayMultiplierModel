0007e8e95ddd6ca81f73d3bd2be95ab4576a4950
sketchturner <o.titomyr@gmail.com>
2014-10-21 14:27:54 +0300

    Unit test edited

---
+++ MultiplierTest/MultiplierModuleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MatrixMultiplier;
+
+namespace MultiplierTest
+{
+    [TestClass]
+    public class MultiplierModuleTest
+    {
+        [TestMethod]
+        public void FullShortTest()
+        {
+            long A = (long)Math.Pow(2, 32)-103832, B = A-20620;
+            int width = 32, truncated = 25;
+            Multiplier mul = new Multiplier(A, B, width, truncated);
+            double actual = mul.ShortMultiply();
+            double expected = mul.Multiply();
+            Assert.AreEqual(expected, actual, Math.Pow(2, width), "Incorrect short multiplication");
+        }
+    }
+}
